1f2dea03a0d926be9893f72a1636997006ffe5e9
Narayan Rizal <113452869+NRizal0@users.noreply.github.com>
2023-10-10 10:56:55 -0500

    AddRecipeFragments: Added preliminary methods for handling buttons actions and reading text.

--- app/src/main/java/com/example/cookbook/ui/home/AddFragment.java
+++ app/src/main/java/com/example/cookbook/ui/home/AddFragment.java
@@ -1,13 +1,20 @@
 package com.example.cookbook.ui.home;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.cookbook.R;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.HashMap;
 
 public class AddFragment extends Fragment {
 
@@ -32,7 +39,53 @@ public class AddFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Initialize your UI components here, for example:
-        // EditText addRecipeName = view.findViewById(R.id.addRecipeName);
+        // Initialize UI components
+        EditText recipeNameEditText = view.findViewById(R.id.editTextRecipeName);
+        EditText ingredientEditText = view.findViewById(R.id.editTextIngredients);
+        LinearLayout ingredientsLayout = view.findViewById(R.id.ingredientsLayout);
+        Button addIngredientButton = view.findViewById(R.id.buttonAddIngredient);
+        Button submitButton = view.findViewById(R.id.buttonSubmit);
+
+        //Handle the click of the "Add Ingredient" button
+        addIngredientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Limit to 15 ingredients
+                if (ingredientsLayout.getChildCount() < 15) {
+                    // Create a new EditText view
+                    EditText newIngredientEditText = new EditText(getContext());
+                    newIngredientEditText.setLayoutParams(new ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT
+                    ));
+                    newIngredientEditText.setHint("Ingredient");
+                    newIngredientEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+
+                    // Add the new EditText to the ingredientsLayout
+                    ingredientsLayout.addView(newIngredientEditText);
+                } else {
+                    // Inform the user about the limit
+                    Snackbar.make(v, "Maximum 15 ingredients allowed", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //Handle the click of the "Submit" button
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a HashMap to store the recipe data
+                HashMap<String, String> recipeData = new HashMap<>();
+                recipeData.put("Recipe Name", recipeNameEditText.getText().toString());
+                recipeData.put("Ingredient", ingredientEditText.getText().toString());
+                //... add other ingredients
+
+                // Display some data back to the user
+                String message = "Recipe Name: " + recipeData.get("Recipe Name") + "\n" +
+                        "First Ingredient: " + recipeData.get("Ingredient");
+                Snackbar.make(v, message, Snackbar.LENGTH_LONG).show();
+
+            }
+        });
     }
 }

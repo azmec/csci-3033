3e4c1e5bb418de1c6206864ef3cb5e401117d262
aldats <pokekights6702@gmail.com>
2023-10-22 15:23:38 -0500

    chore: document category-related code

--- app/src/main/java/com/example/cookbook/database/CategoryTagJoin.java
+++ app/src/main/java/com/example/cookbook/database/CategoryTagJoin.java
@@ -3,35 +3,51 @@ package com.example.cookbook.database;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
 
+/**
+ * Model for a relationship between a category and a tag <i>and</i> the
+ * definition for corresponding SQLite table in the database.
+ *
+ * @author {Carlos Aldana Lira}
+ */
 @Entity(
-        tableName = "category_tag_join",
-        primaryKeys = { "category_id", "tag_id" },
-        foreignKeys = {
-                @ForeignKey(
-                        entity = Category.class,
-                        parentColumns = "uid",
-                        childColumns = "category_id",
-                        onDelete = ForeignKey.CASCADE
-                ),
-                @ForeignKey(
-                        entity = Tag.class,
-                        parentColumns = "uid",
-                        childColumns = "tag_id",
-                        onDelete = ForeignKey.CASCADE
-                )
-        }
+	tableName = "category_tag_join",
+	primaryKeys = { "category_id", "tag_id" },
+	foreignKeys = {
+		@ForeignKey(
+			entity = Category.class,
+			parentColumns = "uid",
+			childColumns = "category_id",
+			onDelete = ForeignKey.CASCADE
+		),
+		@ForeignKey(
+			entity = Tag.class,
+			parentColumns = "uid",
+			childColumns = "tag_id",
+			onDelete = ForeignKey.CASCADE
+		)
+	}
 )
 public class CategoryTagJoin {
-    public int category_id;
-    public int tag_id;
+	/**
+	 * The UID of the related category.
+	 */
+	public int category_id;
 
-    public CategoryTagJoin(int category_id, int tag_id) {
-        this.category_id = category_id;
-        this.tag_id = tag_id;
-    }
+	/**
+	 * The UID of the related tag.
+	 */
+	public int tag_id;
 
-    @Override
-    public String toString() {
-        return String.format("{ category_id: %d, tag_id: %d }", category_id, tag_id);
-    }
+	/**
+	 * Construct a new relation between a category and a tag.
+	 */
+	public CategoryTagJoin(int category_id, int tag_id) {
+		this.category_id = category_id;
+		this.tag_id = tag_id;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("{ category_id: %d, tag_id: %d }", category_id, tag_id);
+	}
 }

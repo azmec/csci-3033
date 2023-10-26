bd060320b9d92f67e0219590a2274577676723e8
Narayan <nr4b@mtmail.mtsu.edu>
2023-10-26 14:06:47 -0500

    Updated addIngredient method in pantry fragment.

--- app/src/main/java/com/example/cookbook/ui/home/PantryFragment.java
+++ app/src/main/java/com/example/cookbook/ui/home/PantryFragment.java
@@ -5,12 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.cookbook.R;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PantryFragment extends Fragment {
 
@@ -31,33 +33,41 @@ public class PantryFragment extends Fragment {
         return inflater.inflate(R.layout.pantry_fragment, container, false);
     }
 
+    private int listIdx = 0;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        EditText editTextIngredientName = view.findViewById(R.id.editTextIngredientName);
         LinearLayout currentIngredientsLayout = view.findViewById(R.id.currentIngredientsLayout);
         LinearLayout addIngredientsLayout = view.findViewById(R.id.addIngredientsLayout);
         Button buttonAddToPantry = view.findViewById(R.id.buttonAddToPantry);
         Button buttonViewPantry = view.findViewById(R.id.buttonViewPantry);
         Button buttonSaveIngredient = view.findViewById(R.id.buttonSaveIngredient);
 
+        //Switching view window to 'add'
         buttonAddToPantry.setOnClickListener(v -> {
             currentIngredientsLayout.setVisibility(View.GONE);
             addIngredientsLayout.setVisibility(View.VISIBLE);
         });
-
+        //Switching view window to 'List'
         buttonViewPantry.setOnClickListener(v -> {
             currentIngredientsLayout.setVisibility(View.VISIBLE);
             addIngredientsLayout.setVisibility(View.GONE);
         });
 
+        //array to store ingredients
+        String[] PantryList = new String[200];
+
+        //Save ingredient to list
         buttonSaveIngredient.setOnClickListener(v -> {
             // Retrieve data from EditText
-            // Save to data source
-            // Update the currentIngredientsLayout with new ingredient
-            // Switch back to viewing mode
+            PantryList[listIdx] = editTextIngredientName.getText().toString();
+            listIdx += 1;
+
             currentIngredientsLayout.setVisibility(View.VISIBLE);
             addIngredientsLayout.setVisibility(View.GONE);
         });
+
     }
 }

4bb974e97a795613c0dd44df51c64c52608d0de7
Narayan Rizal <113452869+NRizal0@users.noreply.github.com>
2023-10-29 21:07:35 -0500

    Finished Pantry UI and button implementation using basic String array.

--- app/src/main/java/com/example/cookbook/ui/home/PantryFragment.java
+++ app/src/main/java/com/example/cookbook/ui/home/PantryFragment.java
@@ -7,12 +7,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import com.example.cookbook.R;
 import com.google.android.material.snackbar.Snackbar;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.Arrays;
 
 
 public class PantryFragment extends Fragment {
@@ -34,13 +39,15 @@ public class PantryFragment extends Fragment {
         return inflater.inflate(R.layout.pantry_fragment, container, false);
     }
 
+    //array to store ingredients
     private int listIdx = 0;
+    String[] PantryList = new String[200];
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         EditText editTextIngredientName = view.findViewById(R.id.editTextIngredientName);
-        LinearLayout currentIngredientsLayout = view.findViewById(R.id.currentIngredientsLayout);
+        LinearLayout currentIngredientsLayout = view.findViewById(R.id.ingredientsDisplayLayout);
         LinearLayout addIngredientsLayout = view.findViewById(R.id.addIngredientsLayout);
         Button buttonAddToPantry = view.findViewById(R.id.buttonAddToPantry);
         Button buttonViewPantry = view.findViewById(R.id.buttonViewPantry);
@@ -51,29 +58,52 @@ public class PantryFragment extends Fragment {
             currentIngredientsLayout.setVisibility(View.GONE);
             addIngredientsLayout.setVisibility(View.VISIBLE);
         });
-        //Switching view window to 'List'
+
+        LinearLayout ingredientsDisplayLayout = view.findViewById(R.id.ingredientsDisplayLayout);
         buttonViewPantry.setOnClickListener(v -> {
+            ingredientsDisplayLayout.removeAllViews(); // Clear previous views
+            for (int i = 0; i < listIdx; i++) {
+                TextView textView = new TextView(getContext());
+                textView.setText(PantryList[i]);
+                textView.setLayoutParams(new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                ));
+                textView.setPadding(16, 8, 16, 8); // optional padding for better visual appeal
+                ingredientsDisplayLayout.addView(textView);
+            }
             currentIngredientsLayout.setVisibility(View.VISIBLE);
             addIngredientsLayout.setVisibility(View.GONE);
         });
-
-        //array to store ingredients
-        String[] PantryList = new String[200];
 
         //Save ingredient to list
         buttonSaveIngredient.setOnClickListener(v -> {
             // Retrieve data from EditText
-            PantryList[listIdx] = editTextIngredientName.getText().toString();
+            String ingredient = editTextIngredientName.getText().toString();
+
+            // Add the ingredient to your PantryList array
+            PantryList[listIdx] = ingredient;
+            listIdx += 1;
+
+            // Display the ingredient in ingredientsDisplayLayout
+            TextView ingredientTextView = new TextView(getContext());
+            ingredientTextView.setText(ingredient);
+            ingredientTextView.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            ));
+            ingredientsDisplayLayout.addView(ingredientTextView);
+
+            // Clear the EditText
+            editTextIngredientName.setText("");
 
             // Display some data back to the user
-            String message = "Ingredient added: " + PantryList[listIdx];
-            Snackbar.make(v, message, Snackbar.LENGTH_LONG).show();
+            //String message = "Ingredient added: " + ingredient;
+            //Snackbar.make(v, message, Snackbar.LENGTH_LONG).show();
 
-            //increment list index
-            listIdx += 1;
+            // Switch views to display the list of ingredients
             currentIngredientsLayout.setVisibility(View.VISIBLE);
             addIngredientsLayout.setVisibility(View.GONE);
         });
-
     }
 }

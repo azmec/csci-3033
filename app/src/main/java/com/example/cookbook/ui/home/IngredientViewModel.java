a1940d799f2238b26659d82c57a648c8d38679ec
Narayan Rizal <113452869+NRizal0@users.noreply.github.com>
2023-11-07 09:20:11 -0600

    Replicated 'RecipeViewModel'

--- app/src/main/java/com/example/cookbook/ui/home/IngredientViewModel.java
+++ app/src/main/java/com/example/cookbook/ui/home/IngredientViewModel.java
@@ -1,22 +1,63 @@
 package com.example.cookbook.ui.home;
 
-import androidx.lifecycle.ViewModel;
-
-import java.util.ArrayList;
+import android.content.Context;
 import java.util.List;
+import java.util.ArrayList;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import com.example.cookbook.database.ingredient.Ingredient;
+import com.example.cookbook.database.ingredient.IngredientRepository;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class IngredientViewModel extends ViewModel {
-    private List<String> data;
 
+    private List<String> data;
     public List<String> getData() {
         if (data == null) {
             data = new ArrayList<String>();
         }
         return data;
     }
-
     public void setData(List<String> newData) {
         data = newData;
+    }
+
+    /** TODO: Link grocery list to be able to send to pantry
+     *
+     *
+     * */
+    private IngredientRepository ingredientRepository;
+    private LiveData<List<Ingredient>> allIngredients;
+
+    public IngredientViewModel() {}
+
+    // Construct and return a view model with an initialized data repository.
+    public IngredientViewModel(Context context) {
+        ingredientRepository = new IngredientRepository(context);
+        allIngredients = LiveDataReactiveStreams.fromPublisher(
+                ingredientRepository.getAll()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .toFlowable()
+        );
+    }
+
+    // Initialize the view model's internal data repository.
+    public void initRepository(Context context) {
+        this.ingredientRepository = new IngredientRepository(context);
+        allIngredients = LiveDataReactiveStreams.fromPublisher(
+                ingredientRepository.getAll()
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .toFlowable()
+        );
+    }
+
+    public LiveData<List<Ingredient>> getAllIngredients() {
+        return allIngredients;
     }
 }
 
